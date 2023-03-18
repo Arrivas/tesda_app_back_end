8c1b344ee0567f9f6164628b7b25afdca1226d3a
unknown <Mavis@Android-UQFNMK3.Android-UQFNMK3>
2023-03-19 02:59:42 +0800

    add borrow

---
+++ models/borrowModel.js
@@ -0,0 +1,24 @@
+const { model, Schema } = require("mongoose");
+
+const borrowSchema = new Schema(
+  {
+    SSP: {
+      type: String,
+      required: [true, "SSP is a required field"],
+    },
+    propertyNo: {
+      type: String,
+      required: [true, "propertyNo is a required field"],
+    },
+    receivedBy: {
+      type: String,
+      required: [true, "receivedBy is a required field"],
+    },
+  },
+  {
+    collection: "borrow",
+    timestamps: true,
+  }
+);
+
+module.exports = model("Borrow", borrowSchema);
